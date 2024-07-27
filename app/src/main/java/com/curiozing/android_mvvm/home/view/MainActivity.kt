bb616866b9945014d4c9cac0da531f27147a7ddc
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-07-27 21:31:48 +0530

    Implemented base setup for the MVVM.

--- app/src/main/java/com/curiozing/android_mvvm/home/view/MainActivity.kt
+++ app/src/main/java/com/curiozing/android_mvvm/home/view/MainActivity.kt
@@ -5,16 +5,23 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.rememberAsyncImagePainter
 import com.curiozing.android_mvvm.home.viewModel.HomeViewModel
@@ -40,7 +47,7 @@ class MainActivity : ComponentActivity() {
 fun AppContent() {
 
     val homeViewModel: HomeViewModel = viewModel()
-    Column(modifier = Modifier.fillMaxSize()) {
+    Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
         val city = homeViewModel.imageData.observeAsState()
 
         Image(
@@ -49,7 +56,8 @@ fun AppContent() {
             contentDescription = "",
             modifier = Modifier.fillMaxWidth()
         )
-        city.value?.name?.let { Text(text = it) }
+        Spacer(modifier = Modifier.height(20.dp))
+        city.value?.name?.let { Text(text = it, style = TextStyle(fontWeight = FontWeight(600), fontSize = 18.sp)) }
 
 
     }
